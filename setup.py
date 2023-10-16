9e43ff35b5ba286323e9d5412e182f7daa7cefdb
kzh <1432245553@qq.com>
2023-10-17 01:16:15 +0800

    version 1.0.0

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='transformers-re',
-    version='0.0.6',
+    version='1.0.0',
     author='kuangzh',
     author_email='1432245553@qq.com',
-    url='https://github.com/kuangkzh/kzhutil',
-    description='RegEx constraint for Language Model output',
+    url='https://github.com/kuangkzh/transformers-re',
+    description='A Regular Expression constraint for Language Models of transformers. With this module, you can force '
+                'the LLMs to generate following your regex. Using regex in tokens and tensors are also implemented in '
+                'this project.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
